c50e7d251d41ff6e7c82985907e0b0f7f206f9b8
husniramdani <husniramdani18@gmail.com>
2020-09-30 17:34:33 +0700

    way too long words

---
+++ codeforces/800/way_too_long_words.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	var s string
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&s)
+		if len(s) > 10 {
+			fmt.Printf("%c%d%c\n", s[0], len(s)-2, s[len(s)-1])
+		} else {
+			fmt.Println(s)
+		}
+	}
+}
