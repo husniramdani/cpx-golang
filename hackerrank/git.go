6ce11bf6117ec719ccbb29ecefa63a1d56f62f7e
husniramdani <husniramdani18@gmail.com>
2023-11-26 19:32:39 +0700

    update git.go

--- hackerrank/git.go
+++ hackerrank/git.go
@@ -101,5 +101,5 @@ func gitCommitCmd(key string, files string){
     fmt.Println("Error :", err)
     return
   }
-  fmt.Printf("Success git commit %s \n\n", messages)
+  fmt.Printf("Success git commit %s \n", messages)
 }
