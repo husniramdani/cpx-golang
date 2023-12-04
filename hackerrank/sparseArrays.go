7d548458dbca3dee9e2ee60991d5c9e2bec35662
husniramdani <husniramdani18@gmail.com>
2023-12-04 19:58:22 +0700

    add sparseArrays.go

---
+++ hackerrank/sparseArrays.go
@@ -0,0 +1,103 @@
+//go:build ignore
+
+// ░░░░░░░░░░░░░░░SPINDYZEL░░░░░░░░░░░░░░░
+// ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
+// ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
+// ░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
+// ░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
+// ░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
+// ░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
+// ░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
+// ░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
+// ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
+// ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
+// ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+func matchingStrings(strings []string, queries []string) []int32 {
+	queryCount := make(map[string]int32)
+	var answer []int32
+
+	for _, str := range strings {
+		queryCount[str] += 1
+	}
+
+	for _, q := range queries {
+		answer = append(answer, queryCount[q])
+	}
+
+	return answer
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	writer := bufio.NewWriterSize(os.Stdout, 16*1024*1024)
+
+	stringsCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var stringsArr []string
+
+	for i := 0; i < int(stringsCount); i++ {
+		stringsItem := readLine(reader)
+		stringsArr = append(stringsArr, stringsItem)
+	}
+
+	queriesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var queries []string
+
+	for i := 0; i < int(queriesCount); i++ {
+		queriesItem := readLine(reader)
+		queries = append(queries, queriesItem)
+	}
+
+	res := matchingStrings(stringsArr, queries)
+
+	for i, resItem := range res {
+		fmt.Fprintf(writer, "%d", resItem)
+
+		if i != len(res)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
