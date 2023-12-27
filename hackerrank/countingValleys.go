bdb6bfc14a4404c95e2e92cd964f6fd05db85b3a
husniramdani <husniramdani18@gmail.com>
2023-12-28 02:17:16 +0700

    add countingValleys.go

---
+++ hackerrank/countingValleys.go
@@ -0,0 +1,95 @@
+
+// ░░░░░░░░░░░░░░░SPINDYZEL░░░░░░░░░░░░░░░
+// ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
+// ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
+// ░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
+// ░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
+// ░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
+// ░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
+// ░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
+// ░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
+// ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
+// ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
+// ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+func countingValleys(steps int32, path string) int32 {
+    // Write your code here
+  var ans int32
+  seaLevel := 0
+  inValley := false
+
+  for _, val := range path {
+    if val == 'U' {
+      seaLevel += 1
+    }
+    if val == 'D' {
+      seaLevel -= 1
+    }
+    if seaLevel < 0 {
+      inValley = true
+    }
+    if inValley && seaLevel >= 0 {
+      ans += 1
+      inValley = false
+    }
+  }
+  return ans
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    stepsTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    steps := int32(stepsTemp)
+
+    path := readLine(reader)
+
+    result := countingValleys(steps, path)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
